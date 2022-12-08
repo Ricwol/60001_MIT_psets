7d03c8e56474f5deb0ff8c330587be4b6b55155a
Ricwol <cedric.r.wolffs@gmail.com>
2022-12-08 12:50:15 +0000

    Add content to get_available_letters

--- pset_02/hangman.py
+++ pset_02/hangman.py
@@ -83,16 +83,18 @@ def get_guessed_word(secret_word, letters_guessed):
     return guessed_word
 
 
-
 def get_available_letters(letters_guessed):
     '''
     letters_guessed: list (of letters), which letters have been guessed so far
     returns: string (of letters), comprised of letters that represents which letters have not
       yet been guessed.
     '''
-    # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
-    
+    available_letters = ""
+
+    for letter in string.ascii_lowercase:
+        if letter not in letters_guessed:
+            available_letters += letter
+    return available_letters
     
 
 def hangman(secret_word):
