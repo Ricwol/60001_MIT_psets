b3f1681228a717965979f6ce008f4f261ab43881
Ricwol <cedric.r.wolffs@gmail.com>
2022-12-08 23:19:38 +0000

    Remove debugging function call to hangman_with_hints
    - Remove debugging function call with secret_word="tact"
    - uncomment secret_word selection and hangman_with_hints games

--- pset_02/hangman.py
+++ pset_02/hangman.py
@@ -522,15 +522,14 @@ if __name__ == "__main__":
 
     # To test part 2, comment out the pass line above and
     # uncomment the following two lines.
-    
+
     # secret_word = choose_word(wordlist)
     # hangman(secret_word)
 
 ###############
-    
+
     # To test part 3 re-comment out the above lines and 
     # uncomment the following two lines. 
-    
-    # secret_word = choose_word(wordlist)
-    # hangman_with_hints(secret_word)
-    hangman_with_hints("tact")
+
+    secret_word = choose_word(wordlist)
+    hangman_with_hints(secret_word)
